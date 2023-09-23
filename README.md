70c63d337f42b40d9778d6b5c700ebe3984465a1
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2023-09-23 13:07:00 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,60 @@
+# ghdepup
+
+A minimal tool to update dependencies based on tags published on github that is easy to integrate in any kind of toolchain.
+
+For a more universal tool -- e.g. sourcing repositories outside of github -- see e.g.:
+
+* https://github.com/dependabot
+* https://github.com/renovatebot/renovate
+
+## project goals
+
+This project aims to provide and update information on tags and releases on github in a way that is easy to integrate into most toolchains and environments. As such it outputs and is configured with a subset of the syntax of [toml](https://github.com/toml-lang/toml) in UTF-8 encoding. So these files should parse as:
+* a [POSIX sh](https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html) environment setup script
+* a form of minimal [INI file](https://en.wikipedia.org/wiki/INI_file) (using double quotes)
+* a form of [Makefile](https://en.wikipedia.org/wiki/Make_(software)) setting up variables
+* stay as much as possible in simple, rust-only build environment
+* enable updates via e.g. github actions to create (and possibly automerge) PRs for dependency updates.
+
+`ghdepup` aims to become self-hosting at some point, that is: it should be able to update its own dependencies. While as of now all those are in `Cargo.toml` and thus can be handled by `cargo update`, this is still considered worthwhile to prove the concept.
+
+`ghdepup` is currently build with dynamic linking. At some point, it might be worthwhile to try to make it statically linked, but the implications -- especially for licensing -- have to be carefully considered.
+
+## project non-goals
+
+The project does NOT want to:
+* read, write or update a wide set of dependency declarations using in many toolchains. If using ghdepup, the toolchain should be adapted to read and write its files as they are very simple.
+* extend sourcing beyond github tags. For open source projects, anything not available on github should be easy to mirror there, thus allowing to use a simple ghdepup tooling.
+* extending understanding versioning beyond [semver](https://semver.org/)
+* stay simple and minimal, thus making it easy to base other work on it (e.g. using tags on gitlab or forejo/codeberg).
+
+## usage
+
+To update the dependencies of a project, set `GITHUB_TOKEN` in the environment with an github API token, and execute the following command:
+
+    ./ghdepup ghdeps1.toml ghdeps2.toml ghdeps3.toml [...] ghversions.toml
+
+in this, there can be any number bigger than zero of config files declaring dependencies like `ghconfig1.toml` here. For each dependency they contain a block:
+
+    HYPER_GH_PROJECT="hyperium/hyper"
+    HYPER_GH_TAG_PREFIX="v"
+    HYPER_GH_VERSION_REQ=">=0.14, <1"
+
+In this `HYPER` is the name of this dependency and:
+* `HYPER_GH_PROJECT` sets where to find the owner and repository on of the dependency. It is required.
+* `HYPER_GH_TAG_PREFIX` sets a possible prefix the tag have before the semantic version. It is not required and assumed to be the empty string when missing.
+* `HYPER_GH_VERSION_REQ` sets a version requirement as per [semver](https://semver.org) to restrict the tags/versions to be considered. It is not required and assumed empty when missing.
+
+The last file given as a command -- `ghversions.toml` in the example above should contain the currently used versions of each dependency, e.g.:
+
+    HYPER_GH_VERSION="0.14.26"
+
+This is not required, but the file has to exist. After parsing all these files, `ghdepup` will look for the newest/best version of each dependency, and replace the file with the name given in the last argument -- `ghversions.toml` in the example -- with its findings. From there it can be picked up by the toolchain or build environment.
+
+## features
+
+Some debugging can be enabled by toogling features to `cargo` in the build. They might be described here later.
+
+## licensing
+
+See LICENSE file in the root directory of the repository.
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,340 @@
+use futures::future::join_all;
 use hyper_tls::HttpsConnector;
 use hyper::Client;
 use hyper::Request;
 use itertools::Itertools;
-use tokio::fs;
+use semver::VersionReq;
 use toml::Table;
-use std::collections::HashMap;
 use std::env;
 use std::str;
 use std::str::FromStr;
+use std::string::FromUtf8Error;
 use serde_json::Value;
 use semver::Version;
 
-fn get_tag_name(entry: &Value) -> Option<&str> {
-    Some(entry.as_object()?["name"].as_str()?)
+enum DepKey {
+    Project,
+    TagPrefix,
+    Version,
+    VersionReq
 }
-async fn parse_tags_json(json_to_parse: &str) -> Option<Vec<String>> {
-    let v : Value = serde_json::from_str(json_to_parse).ok()?;
-    let entries = v.as_array()?;
+impl DepKey {
+    fn as_str(&self) -> &'static str {
+        match self {
+            DepKey::Project => "PROJECT",
+            DepKey::TagPrefix => "TAG_PREFIX",
+            DepKey::Version => "VERSION",
+            DepKey::VersionReq => "VERSION_REQ"
+        }
+    }
+    fn as_full_postfix(&self) -> String {
+        const GH_PREFIX : &str = "GH";
+        return format!("{}_{}", GH_PREFIX, self.as_str());
+    }
+}
+trait GhDepContainer {
+    fn get_ghdep_info(&self, dep: &str, key: DepKey, default: &str) -> String;
+    fn get_all_deps(&self) -> Vec<String>;
+}
+impl GhDepContainer for Table {
+    fn get_ghdep_info(&self, dep: &str, key: DepKey, default: &str) -> String {
+        let default_value = toml::Value::String(default.to_string());
+        let key = format!("{}_{}", dep.to_uppercase(), key.as_full_postfix().as_str());
+        let value = self
+            .get::<String>(&key)
+            .unwrap_or_else(|| &default_value)
+            .as_str()
+            .unwrap_or_else(|| default);
+        return value.to_string();
+    }
+    fn get_all_deps(&self) -> Vec<String>{
+        let postfix = DepKey::Project.as_full_postfix();
+        let chars_to_remove = postfix.len() + 1;
+        return self.keys()
+            .filter(|&k| { k.ends_with(postfix.as_str()) })
+            .map(|k| {
+                return k[..k.len()-chars_to_remove].to_ascii_lowercase()
+            })
+            .collect_vec();
+    }
+}
+struct Dep {
+    name: String,
+    project: String,
+    version_req: Option<VersionReq>,
+    current_version: Option<Version>,
+    tag_prefix: String,
+    available_tags: Vec<String>,
+    available_versions: Vec<Version>,
+    best_version: Option<Version>
+}
+impl Dep {
+    fn from_table(table: &Table, dep: &str) -> Self {
+        let v = Version::from_str(table.get_ghdep_info(
+            dep,
+            DepKey::Version,
+            "").as_str()).ok();
+        let vr = VersionReq::from_str(table.get_ghdep_info(
+            dep,
+            DepKey::VersionReq,
+            "").as_str()).ok();
+        return Self {
+            name: dep.to_string(),
+            project: table.get_ghdep_info(dep, DepKey::Project, ""),
+            version_req: vr,
+            current_version: v,
+            tag_prefix: table.get_ghdep_info(dep, DepKey::TagPrefix, ""),
+            available_tags: vec![],
+            available_versions: vec![],
+            best_version: None
+        }
+    }
+    fn get_versions_from_tags(tags: &Vec<String>, tag_prefix: &str) -> Vec<Version> {
+        tags.iter().filter_map(|tag| {
+            if !tag.starts_with(tag_prefix) {
+                return None;
+            }
+            Version::parse(&tag[tag_prefix.len()..tag.len()]).ok()
+        })
+        .collect_vec()
+    }
+    fn update_versions_from_tags(&mut self) {
+        self.available_versions = Dep::get_versions_from_tags(
+            &self.available_tags,
+            self.tag_prefix.as_str());
+    }
+    fn get_best_version(versions: &Vec<Version>, version_req: &Option<VersionReq>) -> Option<Version> {
+        versions.iter()
+            .filter(|&v| {
+                match &version_req {
+                    None => true,
+                    Some(vr) => vr.matches(v)
+                }
+            })
+            .max()
+            .and_then(|v| Some(v.clone()))
+    }
+    fn update_best_version(&mut self) {
+        self.best_version = Dep::get_best_version(
+            &self.available_versions,
+            &self.version_req);
+    }
+}
+fn to_string(ver: &Option<Version>) -> String {
+    match ver {
+        None => "".to_string(),
+        Some(v) => v.to_string()
+    }
+}
+impl std::fmt::Display for Dep {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}_VERSION=\"{}\"",
+            self.name.to_ascii_uppercase(),
+            to_string(&self.best_version)
+        )
+    }
+}
+impl std::fmt::Debug for Dep {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let available_versions = self.available_versions.iter()
+            .map(|v| v.to_string())
+            .collect_vec();
+        write!(f, "# {}
+# from {}
+# previous version: {}
+# with tags: {}
+# with versions: {}
+{}_VERSION=\"{}\"
+",
+            self.name,
+            self.project,
+            to_string(&self.current_version),
+            self.available_tags.join(", "),
+            available_versions.join(", "),
+            self.name.to_ascii_uppercase(),
+            to_string(&self.best_version)
+        )
+    }
+}
+enum ConfigError {
+    TooFewArgs(usize),
+    NoOutputFile(),
+    ConfigReadError(String),
+    FromUtf8Error(),
+    TomlParseError(toml::de::Error)
+}
+impl std::fmt::Debug for ConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let formatted = match self {
+            Self::TooFewArgs(args) => format!("at least two config files needed, but only {} found.", args),
+            Self::NoOutputFile() => format!("no output file."),
+            Self::ConfigReadError(filename) => format!("error reading config file: {}.", filename),
+            Self::FromUtf8Error() => format!("config is not valid utf8."),
+            Self::TomlParseError(e) => format!("config cant be parsed as toml: {}", e)
+        };
+        write!(f, "{}", formatted)
+    }
+}
+async fn setup_config(args: &Vec<String>) -> Result<(toml::Table, String), ConfigError> {
+    let input_file_names = args.iter()
+        .skip(1)
+        .collect_vec();
+    if input_file_names.len() < 2 {
+        return Err(ConfigError::TooFewArgs(input_file_names.len()));
+    };
+    let output_file_name = args
+        .last()
+        .ok_or(ConfigError::NoOutputFile())?
+        .clone();
+    let mut buf = vec![];
+    for &f in input_file_names.iter() {
+        let mut c = tokio::fs::read(f).await
+            .or(Err(ConfigError::ConfigReadError(f.clone())))?
+            .clone();
+        buf.append(c.as_mut());
+        buf.push(b'\n');
+    }
+    let config_str = String::from_utf8(buf)
+        .or(Err(ConfigError::FromUtf8Error()))?;
+    let config = toml::from_str::<Table>(config_str.as_str())
+        .or_else(|e| Err(ConfigError::TomlParseError(e)))?;
+    Ok((config, output_file_name))
+}
+enum GetTagsError {
+    ExpectedJsonArrayError(),
+    ExpectedJsonName(),
+    ExpectedJsonObjectError(),
+    FromUtf8Error(FromUtf8Error),
+    HyperError(hyper::Error),
+    HyperHttpError(hyper::http::Error),
+    HyperHttpStatusError(hyper::http::StatusCode),
+    JsonParseError()
+}
+impl std::fmt::Display for GetTagsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let formatted = match self {
+            Self::ExpectedJsonArrayError() => format!("json array not found where expected in response."),
+            Self::ExpectedJsonName() => format!("name not found where expected in json response."),
+            Self::ExpectedJsonObjectError() => format!("object not found where expected in json response."),
+            Self::FromUtf8Error(e) => format!("error parsing response as UTF8: {}.", e),
+            Self::HyperError(e) => format!("hyper error: {}.", e),
+            Self::HyperHttpError(e) => format!("hyper http error: {}.", e),
+            Self::HyperHttpStatusError(e) => format!("unexpected http status: {}.", e),
+            Self::JsonParseError() => format!("error parsing json response")
+        };
+        write!(f, "{}", formatted)
+    }
+}
+fn get_tag_name(entry: &Value) -> Result<&str, GetTagsError> {
+    entry
+        .as_object()
+        .ok_or(GetTagsError::ExpectedJsonObjectError())?
+        .get("name")
+        .ok_or(GetTagsError::ExpectedJsonName())?
+        .as_str()
+        .ok_or(GetTagsError::ExpectedJsonName())
+}
+async fn parse_tags_json(json_to_parse: &str) -> Result<Vec<String>, GetTagsError> {
+    let v : Value = serde_json::from_str(json_to_parse)
+        .map_err(|_| GetTagsError::JsonParseError())?;
+    let entries = v.as_array()
+        .ok_or(GetTagsError::ExpectedJsonArrayError())?;
     let str_res = entries
         .iter()
-        .filter_map(|e| {get_tag_name(e)});
-    Some(str_res
+        .filter_map(|e| {get_tag_name(e).ok()});
+    Ok(str_res
         .into_iter()
         .map(str::to_owned)
         .collect_vec())
 }
-async fn find_latest_version(versions: Vec<&str>) -> Option<Version> {
-    versions
-        .into_iter()
-        .filter_map(|v| { Version::parse(v).ok()})
-        .max()
-}
-async fn get_repo_tags_json(repo: &str, token: &str) -> Option<String> {
+async fn get_repo_tags_json(project: &str, token: &str) -> Result<String, GetTagsError> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let url = format!("https://api.github.com/repos/{}/tags", repo);
+    let url = format!("https://api.github.com/repos/{}/tags", project);
     let req = Request::builder()
         .uri(url)
         .header("Accept", "application/vnd.github+json")
         .header("Authorization", format!("Bearer {}", token))
         .header("X-GitHub-Api-Version", "2022-11-28")
-        .header("User-Agent", "depup/1.0")
+        .header("User-Agent", "ghdepup/1.0")
         .body(hyper::Body::empty())
-        .unwrap();
-    let res = client.request(req).await.ok()?;
-    if res.status() != 200 {
-        return None;
+        .map_err(|e| GetTagsError::HyperHttpError(e))?;
+    let res = client.request(req)
+        .await
+        .map_err(|e| GetTagsError::HyperError(e))?;
+    return match res.status().is_success() {
+        false => { Err(GetTagsError::HyperHttpStatusError(res.status())) },
+        true => {
+            let body = res.into_body();
+            let buf = hyper::body::to_bytes(body)
+                .await
+                .map_err(|e| GetTagsError::HyperError(e))?;
+            String::from_utf8(buf.to_vec())
+                .map_err(|e| GetTagsError::FromUtf8Error(e))
+        }
     }
-    let buf = hyper::body::to_bytes(res.into_body()).await.ok()?;
-    String::from_utf8(buf.to_vec()).ok()
 }
-fn get_all_deps(config: &Table) -> Vec<String>{
-    const PROJECT_POSTFIX: &str = "_GH_PROJECT";
-    return config.keys()
-        .filter(|&k| { k.ends_with(PROJECT_POSTFIX)})
-        .map(|k| {
-            return k[..k.len()-PROJECT_POSTFIX.len()].to_ascii_lowercase()
+async fn update_tags_from_gh(dep: &mut Dep, token: &str) -> Result<(), GetTagsError> {
+    let json = get_repo_tags_json(dep.project.as_str(), token)
+        .await?;
+    let tags = parse_tags_json(json.as_str())
+        .await?;
+    dep.available_tags = tags;
+    Ok(())
+}
+fn panic_github_error(updates: &Vec<Result<(), GetTagsError>>) {
+    let fails = updates.iter()
+        .filter_map(|update| -> Option<String> {
+                let e = update.as_ref().err()?;
+                Some(format!("{}", e))
         })
-        .collect_vec();
+        .join("\n");
+    panic!("failed to update tags from github:\n{}", fails);
 }
-fn get_ghdep_info(config: &Table, dep: &str, key: &str) -> String {
-    let key = format!("{}_GH_{}", dep.to_uppercase(), key);
-    let value = config
-        .get::<String>(&key)
-        .unwrap()
-        .as_str()
-        .unwrap();
-    return String::from(value);
+#[cfg(feature="print_debug")]
+async fn print_debug(deps: &Vec<Dep>) {
+    deps.iter().for_each(|dep| {
+        println!("{:?}", dep);
+    });
 }
-fn get_project(config: &Table, dep: &str) -> String {
-    return get_ghdep_info(config, dep, "PROJECT");
+#[cfg(not(feature="print_debug"))]
+async fn print_debug(_: &Vec<Dep>) {}
+
+#[cfg(feature="write_outfile")]
+async fn write_outfile(deps: &Vec<Dep>, outfile: &str) {
+    let formatted = deps.iter()
+        .map(|d| format!("{}\r\n", d))
+        .reduce(|acc, el| acc + el.as_str())
+        .unwrap_or("".to_string());
+    tokio::fs::write(outfile, formatted.as_bytes())
+        .await
+        .expect("fatal: unable to write updated file.")
 }
-fn get_tag_prefix(config: &Table, dep: &str) -> String {
-    return get_ghdep_info(config, dep, "TAG_PREFIX");
-}
-fn get_version_req(config: &Table, dep: &str) -> String {
-    return get_ghdep_info(config, dep, "VERSION_REQ");
-}
-fn get_version(config: &Table, dep: &str) -> String {
-    return get_ghdep_info(config, dep, "VERSION");
-}
+#[cfg(not(feature="write_outfile"))]
+async fn write_outfile(_: &Vec<Dep>, _: &str) {}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let mut token = env::var("GITHUB_TOKEN").unwrap();
+    let mut token = env::var("GITHUB_TOKEN").expect("fatal: GITHUB_TOKEN environment variable not set.");
     token.pop();
+    let config_result : Result<(toml::Table, String), ConfigError>= setup_config(&env::args().collect_vec()).await;
+    let (config, outfile) = config_result.unwrap();
+    let mut deps = config.get_all_deps().iter()
+        .map(|depname| Dep::from_table(&config, depname))
+        .collect_vec();
+    let updates = deps.iter_mut().map(|dep| {
+        update_tags_from_gh(dep, token.as_str())
+    });
+    let updates = join_all(updates).await.into_iter().collect_vec();
+    if !updates.iter().all(|result| result.is_ok()) {
+        panic_github_error(&updates);
+    }
+    deps.iter_mut().for_each(|dep| {
+        dep.update_versions_from_tags();
+        dep.update_best_version();
+    });
+    print_debug(&deps).await;
+    write_outfile(&deps, outfile.as_str()).await;
     Ok(())
 }
 #[cfg(test)]
@@ -92,12 +343,6 @@ mod tests {
     use toml::Table;
 
     use super::*;
-    #[tokio::test]
-    async fn test_find_latest_version() {
-        let versions = vec!["1.2.3"];
-        let latest = find_latest_version(versions).await.unwrap();
-        assert_eq!(latest, Version::parse("1.2.3").unwrap());
-    }
     #[tokio::test]
     async fn test_parse_tags_json() {
         let json = "
@@ -124,7 +369,7 @@ mod tests {
         }
         ]";
         let expected = "1.0.0, 1.2.3";
-        let actual = parse_tags_json(json).await.expect("This should parse.");
+        let actual = parse_tags_json(json).await.ok().expect("this should parse");
         assert_eq!(join(actual.iter(), ", "), expected);
     }
     static CONFIG_CONTENT : &str = "
@@ -133,26 +378,55 @@ HYPER_GH_PROJECT=\"hyperium/hyper\"
 HYPER_GH_TAG_PREFIX=\"v\"
 HYPER_GH_VERSION_REQ=\">=0.14, <1\"
 HYPER_GH_VERSION=\"0.14.26\"
+
+HYPER_TLS_GH_PROJECT=\"hyperium/hyper-tls\"
+HYPER_TLS_GH_TAG_PREFIX=\"v\"
+HYPER_TLS_GH_VERSION_REQ=\">=0.5\"
+HYPER_TLS_GH_VERSION=\"0.5.0\"
     ";
     #[tokio::test]
     async fn test_parse_config() {
         let config = toml::from_str::<Table>(CONFIG_CONTENT)
             .expect("should parse");
         assert_eq!(
-            get_project(&config, "hyper").as_str(),
-            "hyperium/hyper");
+            join(config.get_all_deps(), ", "),
+            "hyper, hyper_tls"
+        );
+        let hyper = Dep::from_table(&config, "hyper");
+        assert_eq!(hyper.name, "hyper");
+        assert_eq!(hyper.project, "hyperium/hyper");
+        assert_eq!(hyper.tag_prefix, "v");
+        assert_eq!(hyper.version_req, VersionReq::parse(">=0.14, <1").ok());
+        assert_eq!(hyper.available_versions.len(), 0);
+        assert_eq!(hyper.available_tags.len(), 0);
         assert_eq!(
-            get_tag_prefix(&config, "hyper").as_str(),
-            "v");
+            hyper.current_version,
+            Some(Version::parse("0.14.26").expect("should parse")));
+        assert_eq!(hyper.best_version, None)
+    }
+    #[tokio::test]
+    async fn test_get_versions_from_tags() {
+        let tags =vec![
+                "foo111",
+                "bar2",
+                "v1.2.3",
+                "2.3.4",
+                "v3.4.5"].iter().map(|&s| {s.to_string()}).collect_vec();
+        let versions = Dep::get_versions_from_tags(&tags, "v");
         assert_eq!(
-            get_version_req(&config, "hyper").as_str(),
-            ">=0.14, <1");
+            join(versions, ", "),
+            "1.2.3, 3.4.5");
+    }
+    #[tokio::test]
+    async fn test_get_best_version() {
+        let versions =vec![
+                "1.2.3",
+                "3.4.0",
+                "3.4.5",
+                "4.5.6"].iter().map(|&s| {Version::parse(s).unwrap()}).collect_vec();
+        let version_req = VersionReq::from_str(">=3, <4").ok();
         assert_eq!(
-            get_version(&config, "hyper").as_str(),
-            "0.14.26");
-        assert_eq!(
-            join(get_all_deps(&config), ", "),
-            "hyper"
-        );   
+            Dep::get_best_version(&versions, &version_req),
+            Version::parse("3.4.5").ok());
     }
 }
