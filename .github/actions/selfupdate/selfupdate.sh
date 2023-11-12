d0b5328925a6fa96eb16cc34f93320f9e23e9fdb
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2023-11-13 00:25:56 +0100

    debug selfupdate

--- .github/actions/selfupdate/selfupdate.sh
+++ .github/actions/selfupdate/selfupdate.sh
@@ -3,5 +3,7 @@ cargo run --release ghdeps.toml ghversions.toml
 cd .github/actions/selfupdate
 cargo run --release ../../../ghversions.toml
 cd ../../..
+pwd && ls -a
+/usr/bin/git config --global --add safe.directory /github/workspace
 git diff
 git commit -m "update dependencies"
